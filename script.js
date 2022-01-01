8afc811b1623cfe1110518515df0beecd30ea238
GaJin Kim <gajin2000@gmail.com>
2022-01-01 16:58:23 -0600

    Finished part 2

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-alert('test alert');
+var age = 21;
+var firstName = "David";
+var lastName = "Beckham";
 
-console.log('test console log');
+var online = true;
 
+var whatever;
+whatever = null;
+
+age+=1;
+var fullName = firstName + " " + lastName; // can't use comma to concatenate strings
+
+console.log('Your age is', age);
+console.log('Hello',firstName,lastName);
+
+console.log('Are you online?:',online);
+
+console.log(whatever);
+console.log('Full name is', fullName);
+
+// let = limited scope (i.e. used for for loops)
+// const = constant variable that can't be changed (e.g. const pi = 3.1415;)
