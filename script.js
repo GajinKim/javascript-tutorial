da7a810f07312ca16d2508210cc842b31eaae29b
GaJin Kim <gajin2000@gmail.com>
2022-01-01 18:30:35 -0600

    Finished part 5

--- script.js
+++ script.js
@@ -7,5 +7,23 @@ document.getElementById("myButton").onclick = () => {
     console.log('hello, my name is', myName);
 }
 
+
+var age = 'a string';
+age = Number(age); // converts to NaN
+age = 1;
+age+=1;
+console.log('age is', age);
+console.log(typeof age);
+
+age = String(age);
+console.log('age is', age);
+console.log(typeof age);
+
+// 0, "", null, undefined, NaN convert to false
+// everything else converts to true
+age = Boolean(age);
+console.log('age is', age);
+console.log(typeof age);
+
 // let = limited scope (i.e. used for for loops)
 // const = constant variable that can't be changed (e.g. const pi = 3.1415;)
