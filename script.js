23c2983e120de930822243a5dd3d184778a397f7
GaJin Kim <gajin2000@gmail.com>
2022-01-01 18:23:34 -0600

    Finished part 4

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
-var age = 21;
-var firstName = "David";
-var lastName = "Beckham";
 
-var online = true;
+// creates an alert requesting a user input
+// var myName = window.prompt("What's your name?");
 
-var whatever;
-whatever = null;
-
-age+=1;
-var fullName = firstName + " " + lastName; // can't use comma to concatenate strings
-
-console.log('Your age is', age);
-console.log('Hello',firstName,lastName);
-
-console.log('Are you online?:',online);
-
-console.log(whatever);
-console.log('Full name is', fullName);
+document.getElementById("myButton").onclick = () => {
+    var myName = document.getElementById("myText").value;
+    console.log('hello, my name is', myName);
+}
 
 // let = limited scope (i.e. used for for loops)
 // const = constant variable that can't be changed (e.g. const pi = 3.1415;)
