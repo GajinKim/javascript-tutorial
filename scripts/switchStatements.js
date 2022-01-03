4cd94ad5b9c0bdea98cac21bb9380042e7feb679
GaJin Kim <gajin2000@gmail.com>
2022-01-02 18:36:18 -0600

    Finished part 10

---
+++ scripts/switchStatements.js
@@ -0,0 +1,19 @@
+var grade = "B";
+
+switch(grade) {
+    case "A": 
+        console.log("A is Poggers");
+        break;
+    case "B":
+        console.log("B is Pretty Poggers");
+        break;
+    case "C":
+    case "D":
+        console.log("C or D is not poggers");
+        break;
+    case "F":
+        console.log("F is Really not poggers");
+        break;
+    default:
+        console.log(grade,"is not a letter grade!");
+};
