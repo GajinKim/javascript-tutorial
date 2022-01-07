8563659ec2f1d3e29ae61ff1841964e75a297935
GaJin Kim <gajin2000@gmail.com>
2022-01-07 17:52:55 -0600

    Finished part 16, 17

---
+++ scripts/math.js
@@ -0,0 +1,14 @@
+console.log(Math.round(3.5));
+console.log(Math.round(3.49));
+console.log(Math.floor(3.99));
+
+
+document.getElementById('myButton').onclick = () => {
+    console.log(Math.random());
+}
+
+var randNum;
+
+// randNum = Math.floor(Math.random() * 6); // 0 - 5 (inclusive)
+// randNum = Math.floor(Math.random() * 100) + 1; // 1 - 100 (inclusive);
+
